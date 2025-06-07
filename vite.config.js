e8f379ea05e8f4ee4545f469bea64961472c3551
sainikhil <madupusainikhil@gmail.com>
2025-06-07 08:07:36 +0530

    Add/update vercel.json for correct Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: './',
   build: {
-    "buildCommand": "npm run build",
-    "outputDirectory": "dist",
-    "rewrites": [
-      { "source": "/(.*)", "destination": "/index.html" }
-    ]
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true
   }
 });
